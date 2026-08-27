853664ea599a7d2aec01046258a36352508b851f
vkonchada1 <vkonchada1@stu.naperville203.org>
2026-08-27 14:09:34 -0500

    idk
    idk

--- Chapter 2/Lessons/Prize/Prize/PrizeGenerator.java
+++ Chapter 2/Lessons/Prize/Prize/PrizeGenerator.java
@@ -1,4 +1,3 @@
-
 /**
  * Generate a random cash prize.
  * 
@@ -21,16 +20,30 @@ public class PrizeGenerator
         String name= scan.nextLine();
         
         System.out.print("How much money do you have? $");
-        double starBalance = scan.nextDouble();
+        double startBalance = scan.nextDouble();
         
         //Generate a random cash prize from $10 - $100
         //Math.random generates a number between 0 (inclusive) and 1 (exclusive)
         //(Math.random() * 91) generates a number between 0 and 90.999999
         //(int)(Math.random() * 91) generates a number 0 and 90
-        //10 + (int)(Math.random() * 91) generates a number between 10 
+        //10 + (int)(Math.random() * 91) generates a number between 10 and 100
         int prizeAmount = 10 + (int)(Math.random() * 91);
 
         //Generate a random fee between $1 and $5
         int fee = 1 + (int)(Math.random() * 5);
+        
+        //int values are automatically promoted to double
+        //This is allowed because no information is lost
+        double finalBalance = startBalance = prizeAmount - fee;
+        
+        System.out.println("\n Congratulations " + name + "! Here is your statement:");
+        System.out.println("=======================================================");
+        
+        System.out.printf("%-25s %s%n", "Starting Balance", money.format(startBalance));
+        System.out.printf("%-25s %s%n", "Prize Money", money.format(prizeAmount));
+        System.out.printf("%-25s %s%n", "Processing Fee", money.format(fee));
+        System.out.println("-----------------------------------------------------------");
+        System.out.printf("%-25s %s%n", "Final Balance", money.format(finalBalance));
+        System.out.println("=======================================================");
     }
 }
